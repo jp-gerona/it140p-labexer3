0a41a06608ec0b02348579fd96f6ac7418ba3fa6
jp-gerona <julianpetergerona@gmail.com>
2025-05-22 02:27:42 +0800

    test: updated and added tests

--- tests/Acceptance/CoursePortalCest.php
+++ tests/Acceptance/CoursePortalCest.php
@@ -16,13 +16,12 @@ final class CoursePortalCest
 
     public function GetCoursesSuccess(AcceptanceTester $I): void
     {
-        // Write your tests here. All `public` methods will be executed as tests.
         $I->wantTo('Submit a student name and view their courses');
 
         $I->amOnPage('/index.php');
-
         $I->fillField('studentName', 'Julian Peter Gerona');
-        $I->click('Submit');
+        $I->wait(2);
+        $I->click('Get courses');
 
         $I->see('Student Information');
         $I->see('Julian Peter Gerona');
@@ -33,6 +32,36 @@ final class CoursePortalCest
         $I->see('Application Development and Emerging Technologies');
         $I->see('IT145');
         $I->see('System Integration and Architectures');
+        $I->see('CS198L');
+        $I->see('Comprehensive Examination Module');
+        $I->see('IT190-2P');
+        $I->see('Networks and Security (Paired)');
+        $I->see('IT190-3P');
+        $I->see('Virtualization and Cloud Security (Paired)');
+        $I->wait(5);
+    }
+
+    public function GetCoursesAnotherSuccess(AcceptanceTester $I): void
+    {
+        $I->wantTo('Submit a student name and view their courses');
+
+        $I->amOnPage('/index.php');
+        $I->fillField('studentName', 'Luis Gerard Tiongco');
+        $I->wait(2);
+        $I->click('Get courses');
+
+        $I->see('Student Information');
+        $I->see('Luis Gerard Tiongco');
+        $I->see('2022152009');
+        $I->see('Bachelor of Science in Information Technology');
+
+        $I->see('IT140P');
+        $I->see('Application Development and Emerging Technologies');
+        $I->see('IT145');
+        $I->see('System Integration and Architectures');
+        $I->see('CS198L');
+        $I->see('Comprehensive Examination Module');
+        $I->wait(5);
     }
 
     public function GetCoursesFailure(AcceptanceTester $I): void
@@ -40,10 +69,42 @@ final class CoursePortalCest
         $I->wantTo('Submit a student name that does not exist');
 
         $I->amOnPage('/index.php');
-
         $I->fillField('studentName', 'Belen Ladesma');
-        $I->click('Submit');
+        $I->wait(2);
+        $I->click('Get courses');
 
-        $I->see('Record not found for: Belen Ladesma');
+        $I->see('Oops!');
+        $I->see('No courses were found for Belen Ladesma');
+        $I->wait(5);
+    }
+
+    public function GetCoursesNoCourses(AcceptanceTester $I): void
+    {
+        $I->wantTo('Submit a student name that exists but has no courses');
+
+        $I->amOnPage('/index.php');
+        $I->fillField('studentName', 'Christian Kerby Salandanan');
+        $I->wait(2);
+        $I->click('Get courses');
+
+        $I->see('Student Information');
+        $I->see('Christian Kerby Salandanan');
+        $I->see('It seems that there are no 3T courses enrolled for this student.');
+        $I->wait(5);
+    }
+
+    public function GetCoursesNoInput(AcceptanceTester $I): void
+    {
+        $I->wantTo('Submit a student name that is empty');
+
+        $I->amOnPage('/index.php');
+        $I->fillField('studentName', '');
+        $I->wait(2);
+        $I->click('Get courses');
+
+        $I->dontSee('Student Information');
+        $I->dontSee('Courses Taken');
+        $I->dontSee('XML Response');
+        $I->wait(5);
     }
 }
